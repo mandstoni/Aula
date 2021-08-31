368db8608da7ea4a1b02bda0e032edffc3b7be95
mandstoni <mdstoni@gmail.com>
2021-08-30 21:12:31 -0300

    Realizando atividade modelo de aula 30-08-21 criando api dontNet Core

---
+++ Aula300821API/Controllers/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula300821API.Controllers.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telephone { get; set; }
+    }
+}
---
+++ Aula300821API/Data/Aula300821APIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Aula300821API.Controllers.Model;
+
+namespace Aula300821API.Models
+{
+    public class Aula300821APIContext : DbContext
+    {
+        public Aula300821APIContext (DbContextOptions<Aula300821APIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aula300821API.Controllers.Model.Person> Person { get; set; }
+    }
+}
